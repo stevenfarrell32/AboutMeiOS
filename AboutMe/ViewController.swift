56e2f1c621697f196e2e8e3c116f2d9aff8f281b
Joseph McLaughlin <joseph.mclaughlin@lambdaschool.com>
2019-07-17 19:35:40 -0600

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Joe (Lambda) on 7/17/19.
+//  Copyright © 2019 Joe (Lambda). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
